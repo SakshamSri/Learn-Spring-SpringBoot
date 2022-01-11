177cc9547b4b4250e6cc9d7ae21c2ff12c836af7
Saksham Srivastava <34575711+SakshamSri@users.noreply.github.com>
2022-01-11 18:00:08 +0530

    learn-springboot-withRest initial commit

---
+++ Learn-SpringBoot-withRest/Learn-SpringBoot-withRest/src/main/java/com/learnspringboot/rest/LearnSpringBootwithRest/LearnSpringBootWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.rest.LearnSpringBootwithRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootWithRestApplication.class, args);
+	}
+
+}
---
+++ Learn-SpringBoot-withRest/Learn-SpringBoot-withRest/src/test/java/com/learnspringboot/rest/LearnSpringBootwithRest/LearnSpringBootWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.rest.LearnSpringBootwithRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
