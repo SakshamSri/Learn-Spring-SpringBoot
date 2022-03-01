aa40b2ecfa180e742828ab375af066c7542b0f92
Saksham Srivastava <34575711+SakshamSri@users.noreply.github.com>
2022-03-01 18:06:54 +0530

    change root dir

---
+++ Microservices/Learn-Microservices-Springboot-Limits-Service/Learn-Microservices-Springboot-Limits-Service/src/main/java/com/microservices/LearnMicroservicesSpringbootLimitsService/LearnMicroservicesSpringbootLimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.LearnMicroservicesSpringbootLimitsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMicroservicesSpringbootLimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMicroservicesSpringbootLimitsServiceApplication.class, args);
+	}
+
+}
