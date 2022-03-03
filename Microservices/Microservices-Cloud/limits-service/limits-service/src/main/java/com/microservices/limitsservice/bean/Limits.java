6977b125dc88bc1af2f265a5c744cdef2b1c12fe
Saksham Srivastava <34575711+SakshamSri@users.noreply.github.com>
2022-03-03 18:00:56 +0530

    currency exchange

---
+++ Microservices/Microservices-Cloud/limits-service/limits-service/src/main/java/com/microservices/limitsservice/bean/Limits.java
@@ -0,0 +1,32 @@
+package com.microservices.limitsservice.bean;
+
+public class Limits {
+	private int minimum;
+	private int maximum;
+
+	public Limits(int minimum, int maximum) {
+		super();
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+
+	protected Limits() {
+		super();
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+}
--- Practice-Coding-Interview/src/coding/strings/OneDotFiveOneAway.java
+++ Practice-Coding-Interview/src/coding/strings/OneDotFiveOneAway.java
@@ -1,5 +1,7 @@
 package coding.strings;
 
+import java.io.IOException;
+
 import helper.fast.io.Reader;
 
 /*
@@ -14,9 +16,55 @@ pale, bake -> false
  */
 public class OneDotFiveOneAway {
 
-	public static void main(String[] args) {
-		Reader reader = new Reader();
+	private static boolean oneReplaceAway(String str1, String str2) {
+		boolean diffChars = false;
+		for (int i = 0; i < str1.length(); i++) {
+			if (str1.charAt(i) != str2.charAt(i)) {
+				if (diffChars) {
+					return false;
+				}
+				diffChars = true;
+			}
+		}
+		return true;
+	}
 
+	private static boolean oneInsertAway(String str1, String str2) {
+		int ind1 = 0, ind2 = 0;
+		while (ind1 < str1.length() && ind2 < str2.length()) {
+			if (str1.charAt(ind1) != str2.charAt(ind2)) {
+				if (ind1 != ind2) {
+					return false;
+				}
+				ind2++;
+			} else {
+				ind1++;
+				ind2++;
+			}
+		}
+		return true;
+	}
+
+	private static boolean doOperation(String str1, String str2) {
+		if (str1.length() == str2.length()) {
+			return oneReplaceAway(str1, str2);
+		} else if (str1.length() + 1 == str2.length()) {
+			return oneInsertAway(str1, str2);
+		} else if (str1.length() == str2.length() + 1) {
+			return oneInsertAway(str2, str1);
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Reader reader = new Reader();
+		// edit
+		// replace = ~insert
+		String string1 = reader.readLine();
+		String string2 = reader.readLine();
+
+		boolean res = doOperation(string1, string2);
+		System.out.println(res);
 	}
 
 }
